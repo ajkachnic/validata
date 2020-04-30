6fe57394870a6ace5574c05a315fdb1ceb5580d6
Andrew Kachnic <ajkachnic@protonmail.com>
2020-04-30 12:36:36 -0400

    :bug: I added type clarification
    I made the program type safe

--- lib/index.ts
+++ lib/index.ts
@@ -9,40 +9,64 @@ interface Config {
 
   max?: number;
   min?: number;
-
 }
 export function isEmail(value: string): boolean {
-  return regex.email.test(value);
+  if (typeof value == "string") {
+    return regex.email.test(value);
+  }
+  return false;
 }
 export function isUrl(value: string): boolean {
-  return regex.url.test(value);
+  if (typeof value == "string") {
+    return regex.url.test(value);
+  }
+  return false;
 }
 export function isHex(value: string): boolean {
-  return regex.hex.test(value);
+  if (typeof value == "string") {
+    return regex.hex.test(value);
+  }
+  return false;
 }
 export function isIp(value: string): boolean {
-  return regex.ip.test(value);
+  if (typeof value == "string") {
+    return regex.ip.test(value);
+  }
+  return false;
 }
 export function isPhone(value: string): boolean {
-  return regex.phone.test(value);
+  if (typeof value == "string") {
+    return regex.phone.test(value);
+  }
+  return false;
 }
 export function isEmpty(value: string): boolean {
-  return value.length == 0;
+  if (typeof value == "string") {
+    return value.length == 0;
+  }
+  return false;
 }
 export function fitsMax(value: string, max: number): boolean {
-  return value.length <= max;
+  if (typeof value == "string") {
+    return value.length <= max;
+  }
+  return false;
 }
 export function fitsMin(value: string, min: number): boolean {
-  return value.length >= min;
+  if (typeof value == "string") {
+    return value.length >= min;
+  }
+  return false;
 }
 
 export default function validate(value: string, config: Config) {
-  if(config.isEmail == true && !isEmail(value)) return false
-  if (config.isUrl == true && !isUrl(value)) return false
+  if (typeof value != "string") return false;
+  if (config.isEmail == true && !isEmail(value)) return false;
+  if (config.isUrl == true && !isUrl(value)) return false;
   if (config.isHex == true && !isHex(value)) return false;
   if (config.isIp == true && !isIp(value)) return false;
   if (config.isEmpty == true && !isEmpty(value)) return false;
-  if(config.max && !fitsMax(value, config.max)) return false;
+  if (config.max && !fitsMax(value, config.max)) return false;
   if (config.min && !fitsMin(value, config.min)) return false;
 
   return true;
